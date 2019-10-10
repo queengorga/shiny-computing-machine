4db56c0a43502b1c5a0587d65e3617479e78b188
Victoria Allen <vickmae.allen@gmail.com>
2019-10-09 18:07:52 -0700

    Lesson 022 - Asking good programming questions

--- programming-questions.py
+++ programming-questions.py
@@ -1,2 +1,4 @@
 # Title - include error message
-# Highlight the code
+# Highlight the code - include entire code; use texts
+# Highlight the error - include entire error message
+# Include expected output
