438abbdec5b37cf583d0d4d7c9dcf634246b3abf
Victoria Allen <vickmae.allen@gmail.com>
2019-10-12 18:31:38 -0700

    Lesson 023 - Exception handling; ZeroDivisionError

--- exception-handling.py
+++ exception-handling.py
@@ -1,8 +1,8 @@
 def divide(a, b):
     try:
         return a / b
-    except:
+    except ZeroDivisionError:
         return "Zero division is meaningless"
 
-print(divide(1, 0))
+print(divide(1, 1))
 print("End of program")
