f95fbc803a1a0d77960f2c6a3c6da184200b69e9
Victoria Allen <vickmae.allen@gmail.com>
2019-10-19 17:42:38 -0700

    Lesson 027 - Functions: Advanced-NoneType

--- functions-advancedfeatures.py
+++ functions-advancedfeatures.py
@@ -1,5 +1,5 @@
 def minutes_to_hours(seconds, minutes=70):
     hours = minutes / 60 + seconds / 3600
-    return hours
+    print(hours)
 
-print(minutes_to_hours(70))
+minutes_to_hours(300, 200) + 10
