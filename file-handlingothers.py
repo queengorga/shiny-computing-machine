e2b33ce7ff5233a8524ab5e9e5351c263d164246
Victoria Allen <vickmae.allen@gmail.com>
2019-11-04 17:28:17 -0800

    Lesson 064 - Other file handling methods

--- file-handlingothers.py
+++ file-handlingothers.py
@@ -1,3 +1,4 @@
 # r Opens a file for reading only. The file pointer placed at the beginning of the file. This is the default mode.
 # r+ Opens a file for both reading and writing. The file pointer placed at the beginning of the file.
-# w+ Opens a file for both writing and reading. Overwrites the existing file if the file exists. If the file doesn't exist, creates a new file for reading and writing.
+# w+ Opens a file for both writing and reading. Overwrites the existing file if the file exists. If the file doesn't exist, it creates a new file for reading and writing.
+# a Opens a file for appending. The file pointer is at the end of the file if the file exists. That is, the file is in the append mode. If the file doesn't exists, it creates a new file for writing.
