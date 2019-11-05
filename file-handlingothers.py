d742eb5f2fcfc3668ff54564a4781c476cf37e91
Victoria Allen <vickmae.allen@gmail.com>
2019-11-04 17:25:36 -0800

    Lesson 064 - Other file handing methods

--- file-handlingothers.py
+++ file-handlingothers.py
@@ -1,1 +1,2 @@
 # r Opens a file for reading only. The file pointer placed at the beginning of the file. This is the default mode.
+# r+ Opens a file for both reading and writing. The file pointer placed at the beginning of the file.
