82f54ce260fb8f48a93195ea4a354563f1b7269b
Victoria Allen <vickmae.allen@gmail.com>
2019-11-30 23:12:41 -0800

    Lesson 83 Making Program Suggest a Similar Word

--- promptinguser-similaritycheck.py
+++ promptinguser-similaritycheck.py
@@ -7,7 +7,7 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif (get_close_matches(w, data.keys())) > 0:
+    elif len(get_close_matches(w, data.keys())) > 0:
         return "Did you mean %s instead?" % get_close_matches(w, data.keys()) [0]
     else:
         return "The word doesn't exist. Please double check it."
@@ -15,5 +15,3 @@ def translate(w):
 word = input("Enter the word: ")
 
 print(translate(word))
-
-# get_close_matches("themandalorian", data.keys(), cutoff=0.5)
