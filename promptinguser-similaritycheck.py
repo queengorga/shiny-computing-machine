fa711c205e5444a95cd0d021ff8c990acc78dff0
Victoria Allen <vickmae.allen@gmail.com>
2019-12-01 17:47:23 -0800

    Lesson 84 Promp User to Confirm Similarity Check

--- promptinguser-similaritycheck.py
+++ promptinguser-similaritycheck.py
@@ -8,7 +8,9 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys()) [0]
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys()[0])
+        if yn == "Y":
+            return data[get_close_matches](w, data.keys())[0]]
     else:
         return "The word doesn't exist. Please double check it."
 
