89398f16d8ddb3370317a6ec7121b4da52db06df
Victoria Allen <vickmae.allen@gmail.com>
2019-12-06 23:39:40 -0800

    Lesson 90 - What is Pandas

--- whatis-pandas.py
+++ whatis-pandas.py
@@ -1,2 +1,4 @@
-# what is pandas
+# what is pandas - a library providing data structures and data analysis tools with Python
+# Pandas allows you to load data from different sources into Python and then use Python code to analyze those data and produce results which can be in the form of tables, text and also visualization
+# With the help of visualization such as Bokeh
 # getting started with pandas
