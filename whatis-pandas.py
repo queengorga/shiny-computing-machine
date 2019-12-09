ad48d127f6d72a842fa45c01e0dfadfd520529ed
Victoria Allen <vickmae.allen@gmail.com>
2019-12-08 23:11:31 -0800

    Lesson 90 - Pandas

--- whatis-pandas.py
+++ whatis-pandas.py
@@ -2,3 +2,6 @@
 # Pandas allows you to load data from different sources into Python and then use Python code to analyze those data and produce results which can be in the form of tables, text and also visualization
 # With the help of visualization such as Bokeh
 # Uses of pandas - loading data from data mining activities such as web scrapping, scrap data from website with Python and store those data in Panda data frames, also use Pandas for analyzing those data instead of Excel.
+# Without numpy (a more low level library than pandas) you cannot use pandas
+# Numpy is the base of pandas
+# 
