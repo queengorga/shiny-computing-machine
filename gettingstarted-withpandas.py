cb538d8bd40fc165a7d9be3a2ed07b5dddd3ee36
Victoria Allen <vickmae.allen@gmail.com>
2019-12-10 22:52:57 -0800

    Lesson 92 - Getting started with Pandas

--- gettingstarted-withpandas.py
+++ gettingstarted-withpandas.py
@@ -1,1 +1,3 @@
-# Pandas - is a library that provides data structures and data analysis for Python
+# Pandas - is a library that provides data structures and data analysis tools for Python
+# Jupiter notebook - python shell, browser based tool where you can write python code, previously known as ipython notebook
+# Pandas using ipython
