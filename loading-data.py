25c14bebf6c1b01d6e51f077ab335bf0a3fd8e41
Victoria Allen <vickmae.allen@gmail.com>
2019-12-17 22:35:01 -0800

    Lesson 95 Loading Data in Python from Files

--- loading-data.py
+++ loading-data.py
@@ -2,4 +2,5 @@
 # import os
 # os.listdr()
 # import pandas 
-# df1=pandas.read_csv("supermarkets.csv")
+# df1=pandas.read_csv("supermarkets.csv"), Headers are bolded by default unless header is assigned the value of none
+# df1.set_index("ID") - will set ID as an index
